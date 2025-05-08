26641f9b980df9ce62694358e4800f738aec33d6
unknown <joseluisql97@alumnos.cei.es>
2025-05-08 11:39:44 +0200

    CampoTexto

--- dia10/ejerciciodia10/src/components/CampoTexto.jsx
+++ dia10/ejerciciodia10/src/components/CampoTexto.jsx
@@ -3,6 +3,21 @@ import React, {useState} from "react";
 const CampoTexto = () => {
     const [texto, setTexto] = useState("");
 
-    
-    
-}
+    const manejarCambio = (e) => {
+        setTexto(e.targer.value);
+    };
+
+    return (
+        <div>
+            <input
+            type="text"
+            value={texto}
+            onChange={manejarCambio}
+            placeholder="Escribe Algo"
+            />
+            <p>Texto ingresado: {texto}</p>
+        </div>
+    );
+};
+
+export default CampoTexto;
